b06f0a4bc6ab5f14f15ecf47fe8ac4b035039a18
JongWon Kim <kjw3829620@naver.com>
2020-08-14 20:29:58 +0900

    완벽한 슬라이드쇼 완성(찐)

--- frontend/iver_proj/src/component/Slider.js
+++ frontend/iver_proj/src/component/Slider.js
@@ -7,13 +7,13 @@ import banner3 from '../image/bannerImage_3.jpg'
 import banner4 from '../image/bannerImage_4.jpg'
 import banner5 from '../image/bannerImage_5.jpg'
 import banner6 from '../image/bannerImage_6.jpg'
-import banner7 from '../image/bannerImage_7.jpg'
-import banner8 from '../image/bannerImage_8.jpg'
 
 class Slider extends React.Component {
 
     componentDidMount (){
         this.Interval();
+        const dot = document.getElementById('dot');
+        dot.childNodes[0].style.backgroundColor = 'red';
     }
 
     state = {
@@ -21,7 +21,8 @@ class Slider extends React.Component {
         pos: 0,
         timer: '',
         slideShowTimer: 3000,
-        dot_position: 0
+        dot_position: 0,
+        preventDot: 0
     }
 
     Interval = () => {
@@ -32,14 +33,25 @@ class Slider extends React.Component {
 
     dotPush = () => {
         const dot = document.getElementById('dot');
-        console.log(dot.childNodes[this.state.dot_position-1], this.state.dot_position);
 
-        if(dot.childNodes[this.state.dot_position-1] != undefined){
+        if(dot.childNodes[this.state.dot_position] != undefined){
             let dotPos = this.state.dot_position;
+           
             if(dotPos < 4){
+                dot.childNodes[dotPos].style.backgroundColor = 'red';
+                dot.childNodes[this.state.preventDot].style.backgroundColor = 'black';
+
+                this.setState({
+                    preventDot: dotPos
+                });
+
                 dotPos = this.state.dot_position+1;
-            } else {
+            }
+            if(dotPos == 4) {
                 dotPos = 0;
+                this.setState({
+                    dot_position: 0
+                });
             }
                 
             this.setState({
@@ -79,11 +91,7 @@ class Slider extends React.Component {
             });
         }
         console.log(this.state.pos);
-    }
-
-    ISPclick = (ele) => {
-        console.log("click ele: ", ele.target);
-    }
+    }    
 
     render() {
 
@@ -107,10 +115,10 @@ class Slider extends React.Component {
 
                 <div className="ISPager">
                     <ul className="ISPList" id="dot">
-                        <li pos='1' onClick={this.ISPclick} key='ISP1' className='ISP'><a href="#"></a></li>
-                        <li pos='2' onClick={this.ISPclick} key='ISP2' className='ISP'><a href="#"></a></li>
-                        <li pos='3' onClick={this.ISPclick} key='ISP3' className='ISP'><a href="#"></a></li>
-                        <li pos='4' onClick={this.ISPclick} key='ISP4' className='ISP'><a href="#"></a></li>
+                        <li pos='1' key='ISP1' className='first ISP'><a href="#"></a></li>
+                        <li pos='2' key='ISP2' className='ISP'><a href="#"></a></li>
+                        <li pos='3' key='ISP3' className='ISP'><a href="#"></a></li>
+                        <li pos='4' key='ISP4' className='ISP'><a href="#"></a></li>
                     </ul>
                 </div>
             </div>
