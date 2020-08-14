5c2dec4b960995b56037849e866090b3aea15ba6
JongWon Kim <kjw3829620@naver.com>
2020-08-14 16:31:18 +0900

    안정적인 자동 스라이드쇼 완성

--- frontend/iver_proj/src/component/Slider.js
+++ frontend/iver_proj/src/component/Slider.js
@@ -10,17 +10,47 @@ import banner6 from '../image/bannerImage_6.jpg'
 import banner7 from '../image/bannerImage_7.jpg'
 import banner8 from '../image/bannerImage_8.jpg'
 
-
 class Slider extends React.Component {
+
+    componentDidMount (){
+        this.Interval();
+    }
 
     state = {
         imgList: [banner1,banner2,banner3,banner4,banner5,banner6],
-        pos: 0
+        pos: 0,
+        timer: '',
+        slideShowTimer: 3000,
+        dot_position: 0
+    }
+
+    Interval = () => {
+        this.state.timer = setTimeout(this.Interval, this.state.slideShowTimer)
+        this.push();
+        this.dotPush();
+    }
+
+    dotPush = () => {
+        const dot = document.getElementById('dot');
+        console.log(dot.childNodes[this.state.dot_position-1], this.state.dot_position);
+
+        if(dot.childNodes[this.state.dot_position-1] != undefined){
+            let dotPos = this.state.dot_position;
+            if(dotPos < 4){
+                dotPos = this.state.dot_position+1;
+            } else {
+                dotPos = 0;
+            }
+                
+            this.setState({
+                dot_position: dotPos
+            });
+        }
     }
 
     push = () => {
         let pos = this.state.pos;
-
+        
         if(pos >= -((this.state.imgList.length-4)*16.66)){
             this.setState({
                 pos: pos - 16.66
@@ -51,6 +81,10 @@ class Slider extends React.Component {
         console.log(this.state.pos);
     }
 
+    ISPclick = (ele) => {
+        console.log("click ele: ", ele.target);
+    }
+
     render() {
 
         const style = {
@@ -67,23 +101,18 @@ class Slider extends React.Component {
                         <img className='img' src={this.state.imgList[2]} alt="banner3"/>
                         <img className='img' src={this.state.imgList[3]} alt="banner4"/>
                         <img className='img' src={this.state.imgList[4]} alt="banner5"/>
-                        <img className='img' src={this.state.imgList[5]} alt="banner6"/>
-                        {/* <img className='img' src={this.state.imgList[6]}/>
-                        <img className='img' src={this.state.imgList[7]}/> */}
+                        <img className='img' src={this.state.imgList[5]} alt="banner6"/>    
                     </div>
                 </div>
 
                 <div className="ISPager">
-                    <ul className="ISPList">
-                        <li className='ISP'><a href="#"></a></li>
-                        <li className='ISP'><a href="#"></a></li>
-                        <li className='ISP'><a href="#"></a></li>
-                        <li className='ISP'><a href="#"></a></li>
+                    <ul className="ISPList" id="dot">
+                        <li pos='1' onClick={this.ISPclick} key='ISP1' className='ISP'><a href="#"></a></li>
+                        <li pos='2' onClick={this.ISPclick} key='ISP2' className='ISP'><a href="#"></a></li>
+                        <li pos='3' onClick={this.ISPclick} key='ISP3' className='ISP'><a href="#"></a></li>
+                        <li pos='4' onClick={this.ISPclick} key='ISP4' className='ISP'><a href="#"></a></li>
                     </ul>
                 </div>
-
-                <button onClick={this.push}>push</button>
-                <button onClick={this.pull}>pull</button>
             </div>
             
         );
