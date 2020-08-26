a5f3b0acce627b495fc9e82ec808d03e5ef6d9d0
JongWon Kim <kjw3829620@naver.com>
2020-08-26 16:24:49 +0900

    best 상품 제작 진행중

---
+++ frontend/iver_project/src/component/BestProduct.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import "./bestProduct.css"
+
+class BestProduct extends React.Component{
+
+    render() {
+        return(
+            <div className="bestProduct_bg">
+                <div className="page_title">
+                    <h1>{this.props.categori + " BEST"}</h1>
+                    <p className="detail">더보기</p>
+                </div>
+                
+                <div className="list_full">
+                    
+                    <ul>
+                        <li className="list_image">
+                            <a style={{margin: "0"}}>
+                                <img className="image" src="https://image.brandi.me/cproduct/2018/12/20/6648788_1545292189_image1_L.jpg"/>
+                            </a>
+                        </li>
+                        <li className="list_seller">
+                            <a style={{margin: "0"}}>카네브로스</a>
+                        </li>
+                        <li className="list_title">
+                            <a style={{margin: "0"}}>[CB] 오버 더블 후드 트...</a>
+                        </li>
+                        <li className="list_price">
+                            <a style={{margin: "0"}}>169,200</a>
+                        </li>
+                    </ul>
+
+                    <ul>
+                        <li className="list_image">
+                            <a style={{margin: "0"}}>
+                                <img className="image" src="https://image.brandi.me/cproduct/2018/12/20/6648788_1545292189_image1_L.jpg"/>
+                            </a>
+                        </li>
+                        <li className="list_seller">
+                            <a style={{margin: "0"}}>카네브로스</a>
+                        </li>
+                        <li className="list_title">
+                            <a style={{margin: "0"}}>[CB] 오버 더블 후드 트...</a>
+                        </li>
+                        <li className="list_price">
+                            <a style={{margin: "0"}}>169,200</a>
+                        </li>
+                    </ul>
+
+                    <ul>
+                        <li className="list_image">
+                            <a style={{margin: "0"}}>
+                                <img className="image" src="https://image.brandi.me/cproduct/2018/12/20/6648788_1545292189_image1_L.jpg"/>
+                            </a>
+                        </li>
+                        <li className="list_seller">
+                            <a style={{margin: "0"}}>카네브로스</a>
+                        </li>
+                        <li className="list_title">
+                            <a style={{margin: "0"}}>[CB] 오버 더블 후드 트...</a>
+                        </li>
+                        <li className="list_price">
+                            <a style={{margin: "0"}}>169,200</a>
+                        </li>
+                    </ul>
+
+                    <ul>
+                        <li className="list_image">
+                            <a style={{margin: "0"}}>
+                                <img className="image" src="https://image.brandi.me/cproduct/2018/12/20/6648788_1545292189_image1_L.jpg"/>
+                            </a>
+                        </li>
+                        <li className="list_seller">
+                            <a style={{margin: "0"}}>카네브로스</a>
+                        </li>
+                        <li className="list_title">
+                            <a style={{margin: "0"}}>[CB] 오버 더블 후드 트...</a>
+                        </li>
+                        <li className="list_price">
+                            <a style={{margin: "0"}}>169,200</a>
+                        </li>
+                    </ul>
+
+                    
+                </div>
+            
+            </div>
+        )
+    }
+
+}
+
+export default BestProduct
