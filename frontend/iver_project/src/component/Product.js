06f51ba4ce63a8fe9c8aa278db843be855017d46
JongWon Kim <kjw3829620@naver.com>
2020-09-02 22:02:26 +0900

    sort 및 상품 리스트 컴포넌트 제작 (css 이쁘게 바꿀 필요 있음)

---
+++ frontend/iver_project/src/component/Product.js
@@ -0,0 +1,75 @@
+import React from "react"
+
+import "./product.css"
+import { Link } from "react-router-dom"
+
+class Product extends React.Component {
+
+    render() {
+        
+        return(
+            <div className="product_Main">
+                <Link to="/"><ul>
+                    <li className="list_image"><img className="image" src="https://image.brandi.me/cproduct/2018/12/20/6648788_1545292189_image1_L.jpg"/></li>
+                    <li className="list_seller">카네브로스</li>
+                    <li className="list_title">[CB] 오버 더블 후드 트...</li>
+                    <li className="list_price">169,200</li>
+                </ul></Link>
+
+                <Link to="/"><ul>
+                    <li className="list_image"><img className="image" src="https://image.brandi.me/cproduct/2018/12/20/6648788_1545292189_image1_L.jpg"/></li>
+                    <li className="list_seller">카네브로스</li>
+                    <li className="list_title">[CB] 오버 더블 후드 트...</li>
+                    <li className="list_price">169,200</li>
+                </ul></Link>
+
+                <Link to="/"><ul>
+                    <li className="list_image"><img className="image" src="https://image.brandi.me/cproduct/2018/12/20/6648788_1545292189_image1_L.jpg"/></li>
+                    <li className="list_seller">카네브로스</li>
+                    <li className="list_title">[CB] 오버 더블 후드 트...</li>
+                    <li className="list_price">169,200</li>
+                </ul></Link>
+
+                <Link to="/"><ul>
+                    <li className="list_image"><img className="image" src="https://image.brandi.me/cproduct/2018/12/20/6648788_1545292189_image1_L.jpg"/></li>
+                    <li className="list_seller">카네브로스</li>
+                    <li className="list_title">[CB] 오버 더블 후드 트...</li>
+                    <li className="list_price">169,200</li>
+                </ul></Link>
+
+                <Link to="/"><ul>
+                    <li className="list_image"><img className="image" src="https://image.brandi.me/cproduct/2018/12/20/6648788_1545292189_image1_L.jpg"/></li>
+                    <li className="list_seller">카네브로스</li>
+                    <li className="list_title">[CB] 오버 더블 후드 트...</li>
+                    <li className="list_price">169,200</li>
+                </ul></Link>
+
+                <Link to="/"><ul>
+                    <li className="list_image"><img className="image" src="https://image.brandi.me/cproduct/2018/12/20/6648788_1545292189_image1_L.jpg"/></li>
+                    <li className="list_seller">카네브로스</li>
+                    <li className="list_title">[CB] 오버 더블 후드 트...</li>
+                    <li className="list_price">169,200</li>
+                </ul></Link>
+
+                <Link to="/"><ul>
+                    <li className="list_image"><img className="image" src="https://image.brandi.me/cproduct/2018/12/20/6648788_1545292189_image1_L.jpg"/></li>
+                    <li className="list_seller">카네브로스</li>
+                    <li className="list_title">[CB] 오버 더블 후드 트...</li>
+                    <li className="list_price">169,200</li>
+                </ul></Link>
+
+                <Link to="/"><ul>
+                    <li className="list_image"><img className="image" src="https://image.brandi.me/cproduct/2018/12/20/6648788_1545292189_image1_L.jpg"/></li>
+                    <li className="list_seller">카네브로스</li>
+                    <li className="list_title">[CB] 오버 더블 후드 트...</li>
+                    <li className="list_price">169,200</li>
+                </ul></Link>
+
+
+            </div>
+        )
+    }
+
+}
+
+export default Product;
--- frontend/iver_project/src/component/middle/ProductPage.js
+++ frontend/iver_project/src/component/middle/ProductPage.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import Product from '../Product'
+
 import './productPage.css'
 import check1 from '../../image/ico_chk.png'
+
 
 class ProductPage extends React.Component{
 
@@ -15,12 +18,21 @@ class ProductPage extends React.Component{
             border: "none",
             width: "37px",
             height: "37px"
-        }
+        },
 
+        sort_status: "인기순",
+        sort_style: {
+            fontWeight: "1000"
+        },
+        sort_default:{
+
+        }
     }
 
-    componentDidUpdate() {
-        console.log(this.state.toggle_chkBox);
+    sort_product = (sort) => {
+        this.setState({
+            sort_status: sort
+        });
     }
 
     chkBox_click = () => {
@@ -47,18 +59,20 @@ class ProductPage extends React.Component{
                         </div>
 
                         <div className="list_sort">
-                            <span>최신순</span>
+                            <span style={this.state.sort_status === "최신순" ? this.state.sort_style : this.state.sort_default } onClick={() => {this.sort_product("최신순")}}>최신순</span>
                             <p>|</p>
-                            <span>인기순</span>
+                            <span style={this.state.sort_status === "인기순" ? this.state.sort_style : this.state.sort_default } onClick={() => {this.sort_product("인기순")}}>인기순</span>
                             <p>|</p>
-                            <span>낮은가격순</span>
+                            <span style={this.state.sort_status === "낮은가격순" ? this.state.sort_style : this.state.sort_default } onClick={() => {this.sort_product("낮은가격순")}}>낮은가격순</span>
                             <p>|</p>
-                            <span>높은가격순</span>
+                            <span style={this.state.sort_status === "높은가격순" ? this.state.sort_style : this.state.sort_default } onClick={() => {this.sort_product("높은가격순")}}>높은가격순</span>
                             <p>|</p>
-                            <span>리뷰순</span>
+                            <span style={this.state.sort_status === "리뷰순" ? this.state.sort_style : this.state.sort_default } onClick={() => {this.sort_product("리뷰순")}}>리뷰순</span>
                         </div>
 
                     </div>
+
+                    <Product></Product>
                 </div>
             </div>
         );
